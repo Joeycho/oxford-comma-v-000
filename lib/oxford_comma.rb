0d64d288f0f3dc9c91355f94100fe4aa1eaf2c05
Joey Cho <cjy12182000@naver.com>
2017-10-20 21:14:51 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,9 +6,9 @@ def oxford_comma(array)
   else
     nstr = array.join(", ")
     narr = nstr.split(" ")
-    narr[array.length-2]=array[array.length-2]+" and "
+    narr[array.length-2]=array[array.length-2]+" and"
 
-    nnarr = narr.join
+    nnarr = narr.join(" ")
   end
 end
 print (oxford_comma(["hi","dog","cat","giraffe"]))
