1846b0cf9fea18b81fe60c931a9f2d839fd0f60c
Joey Cho <cjy12182000@naver.com>
2017-10-20 21:39:05 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,11 +7,12 @@ def oxford_comma(array)
     nstr = array.join(", ")
     narr = nstr.split(", ")
     for i in 0..array.length
+      print(i)
       if i == array.length-2
   #      narr[array.length-2]=array[array.length-2] + ', and'
-         print(array[array.length-2]+', and')
+        # print(array[array.length-2]+', and')
       else
-        narr[i]=array[i] + ','
+        #narr[i]=array[i] + ','
       end
     end
 
